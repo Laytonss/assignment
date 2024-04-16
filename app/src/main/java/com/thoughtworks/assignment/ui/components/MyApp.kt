0e2db5987a4ae22f371c2698868f5d852f616752
Laytonss <jiajun.huang@thoughworks.com>
2024-04-16 17:14:14 +0800

    refactor: delete no use import

--- app/src/main/java/com/thoughtworks/assignment/ui/components/MyApp.kt
+++ app/src/main/java/com/thoughtworks/assignment/ui/components/MyApp.kt
@@ -1,11 +1,7 @@
 package com.thoughtworks.assignment.ui.components
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.CenterAlignedTopAppBar
@@ -19,7 +15,6 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.style.TextAlign
 
 
 enum class BottomNavigationItem {
