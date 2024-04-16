cd8947ee59a6e070ade756c157a5914e8d1f2306
Laytonss <jiajun.huang@thoughworks.com>
2024-04-16 17:13:50 +0800

    feat: add title bar

--- app/src/main/java/com/thoughtworks/assignment/ui/components/MyApp.kt
+++ app/src/main/java/com/thoughtworks/assignment/ui/components/MyApp.kt
@@ -8,23 +8,42 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-
+import androidx.compose.ui.text.style.TextAlign
 
 
 enum class BottomNavigationItem {
     CHATS, CONTACTS, DISCOVER, ME
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MyApp() {
     val (selectedItem, setSelectedItem) = remember { mutableStateOf(BottomNavigationItem.DISCOVER) }
+    val title = when (selectedItem) {
+        BottomNavigationItem.CHATS -> "Chats"
+        BottomNavigationItem.CONTACTS -> "Contacts"
+        BottomNavigationItem.DISCOVER -> "Discover"
+        BottomNavigationItem.ME -> "Me"
+    }
+
     Scaffold(
+        topBar = {
+            CenterAlignedTopAppBar(
+                title = { Text(text = title) },
+                colors = topAppBarColors(containerColor = Color.Black, titleContentColor = Color.White)
+            )
+        },
         bottomBar = {
             AppBottomBar(selectedItem) { newItem -> setSelectedItem(newItem) }
         },
@@ -54,7 +73,7 @@ fun EmptyPage() {
     Box(
         modifier = Modifier
             .fillMaxSize()
-            .background(color = Color.Black)
+            .background(color = Color.Gray)
     ) {
     }
 }
